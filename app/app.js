8a51262d46af37d93e886b82b290be7b07a0fc69
Sara Farkas <slschram1@gmail.com>
2017-01-14 17:41:03 -0600

    more changes made

--- app/app.js
+++ app/app.js
@@ -14,5 +14,5 @@ angular.module('myApp', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/emotion'});
+  $routeProvider.otherwise({redirectTo: '/index.html'});
 }]);
--- app/out/out.js
+++ app/out/out.js
@@ -12,3 +12,13 @@ angular.module('myApp.out', ['ngRoute'])
 .controller('OutCtrl', [function() {
 
 }]);
+
+
+// Anoka County 763-755-3801
+// Carver/Scott Counties 952-442-7601
+// Dakota County 952-891-7171
+// Washington County 651-777-5222
+// Ramsey County, Adults 651-266-7900
+// Ramsey County, Children 651-774-7000
+// Hennepin County, Adults 612-596-1223
+// Hennepin County, Children 612-348-2233
